b61e6821dfc6c239f7c2fe4490056d6feaf5bd41
15starcaller <takanuva@mail.ru>
2019-12-15 12:02:22 +0300

    b/s

--- src/main/java/com/cko/sampleSpringProject/InitData.java
+++ src/main/java/com/cko/sampleSpringProject/InitData.java
@@ -1,13 +1,16 @@
 package com.cko.sampleSpringProject;
 
 import com.cko.sampleSpringProject.dao.FilmDAO;
+import com.cko.sampleSpringProject.dao.ProductDAO;
 import com.cko.sampleSpringProject.model.Authority;
-import com.cko.sampleSpringProject.model.Film;
+import com.cko.sampleSpringProject.model.Product;
 import com.cko.sampleSpringProject.model.User;
+import com.cko.sampleSpringProject.model.Film;
 import com.cko.sampleSpringProject.service.AuthorityService;
 import com.cko.sampleSpringProject.service.SMSCService;
 import com.cko.sampleSpringProject.service.UserService;
 
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -25,47 +28,45 @@ public class InitData {
     AuthorityService authorityService;
 
     @Autowired
-    SMSCService smscSender;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     FilmDAO filmDAO;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    ProductDAO productDAO;
+
+    Faker faker = new Faker();
+
 
     public void initData() {
+        initFilms();
+        initUserAndRoles();
+    }
+    public void initFilms() {
 
 //        smscSender.send_sms("89775548911","TEST MESSAGE",1, "", "", 0, "", "");
-        initUserAndRoles();
-        initFilms();
+        for (int i = 0; i < 10; i++) {
+            Product Product = new Product(faker.food().ingredient(), (int) ( Math.random() * 100 ), (int) ( Math.random() * 50 ));
+            productDAO.save(Product);
+        }
 
-
+        for (int i = 0; i < 10; i++) {
+            Film film = new Film(faker.superhero().name(), i, i * 2);
+            filmDAO.save(film);
+        }
     }
 
-    private void initFilms() {
-        Film film = new Film("Rosomaha",9,18);
-        filmDAO.save(film);
-        Film film1 = new Film("Rosomaha324",9,18);
-        filmDAO.save(film1);
-        Film film2 = new Film("Rosomaha234",9,18);
-        filmDAO.save(film2);
-        Film film3 = new Film("Rosomahagtfhjfgj",9,18);
-        filmDAO.save(film3);
-    }
-
-    private void initUserAndRoles(){
+    private void initUserAndRoles() {
         Authority adminAuthority = new Authority("ROLE_ADMIN");
         Authority userAuthority = new Authority("ROLE_USER");
-        authorityService.insert(adminAuthority);//сохранение в базу
+        authorityService.insert(adminAuthority);
         authorityService.insert(userAuthority);
 
         List<Authority> authorities = new ArrayList<Authority>();
         authorities.add(adminAuthority);
-        for (int i = 0; i < 10; i++) {
+        userService.insert(new User("1@mail.ru", bCryptPasswordEncoder.encode("1"), authorities));
 
-
-            userService.insert(new User(i+"1@mail.ru", bCryptPasswordEncoder.encode(String.valueOf(i)), authorities));
-        }
 
     }
 }
--- src/main/java/com/cko/sampleSpringProject/controller/REST/ProductRestController.java
+++ src/main/java/com/cko/sampleSpringProject/controller/REST/ProductRestController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("api/products")
 public class ProductRestController {
 
     @Autowired
