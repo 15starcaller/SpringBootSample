45356deb5f20c0b84b747d3df9e79f168221dfac
15starcaller <takanuva@mail.ru>
2019-12-22 12:06:35 +0300

    Save changes, Add

--- src/main/resources/static/js/oldFilms.js
+++ src/main/resources/static/js/oldFilms.js
@@ -1,8 +1,4 @@
-
-
-function test () {
-
-
+function test() {
 
 
 }
@@ -10,7 +6,6 @@ function test () {
 $(document).ready(function () {
     $.ajax({
         url: '/api/film/all',
-        type: 'get',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         success: function (films) {//array
@@ -21,15 +16,16 @@ $(document).ready(function () {
                 // console.log(films[i].title)
                 $("#forAdd").before(
                     "<tr>" +
-                    "<td>" +films[i].id +"</td>" +
-                    "<td>" +films[i].title +"</td>" +
-                    "<td>" +films[i].rating +"</td>" +
-                    "<td>" +films[i].age +"</td>" +
+                    "<td>" + films[i].id + "</td>" +
+                    "<td>" + films[i].title + "</td>" +
+                    "<td>" + films[i].rating + "</td>" +
+                    "<td>" + films[i].age + "</td>" +
                     "<td>" +
-                    "<a class=\"btn btn-primary\" href=\"/films/editFilm?id="+films[i].id+"\">Изменить</a>" +
-                    "<a class=\"btn btn-primary\" href=\"/films/deleteFilm?id="+films[i].id+"\">Удалить</a>" +
-                    "</td>"+
-                "</td>")
+                    // "<a class=\"btn btn-primary\" href=\"/films/editFilm?id="+films[i].id+"\">Изменить</a>" +
+                    "<button class=\"btn btn-outline-warning\" data-toggle=\"modal\" data-target=\"#edit-modal\" onclick=\"fillModal(" + films[i].id + ")\">Редактировать</button>" +
+                    "<a class=\"btn btn-primary\" href=\"/films/deleteFilm?id=" + films + [i].id + "\">Удалить</a>" +
+                    "</td>" +
+                    "</td>")
             }
 
         },
@@ -40,23 +36,53 @@ $(document).ready(function () {
 
 })
 
-function addFilm(){ /*ajax наоборот*/
+function fillModal(id) {
+    alert(id);
+    $.ajax({
+        method: 'get',
+        url: '/api/film/get?id=' + id,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (film) {
+            $("#id-edit").val(film.id);
+            $("#id-title").val(film.title);
+            $("#id-rating").val(film.ratingt);
+            $("#id-age").val(film.age);
+        }
+    });
+}
 
-    var title =$("#title").val();
-    var rating =$("#rating").val();
-    var age =$("#age").val();
+function saveEditData() {
+    var id = $("#id-edit").val();
+    var title = $("#id-title").val();
+    var rating = $("#id-rating").val();
+    var age = $("#id-age").val();
 
-    var newProduct ={
-        'title':title,
-        'rating':rating,
-        'age':age
+    var newProduct = {
+        'id': id,
+        'title': title,
+        'rating': rating,
+        'age': age
+    }
+}
+
+function addFilm() { /*ajax наоборот*/
+
+    var title = $("#title").val();
+    var rating = $("#rating").val();
+    var age = $("#age").val();
+
+    var newProduct = {
+        'title': title,
+        'rating': rating,
+        'age': age
     }
     $.ajax({
-        method:"post",
+        method: "post",
         url: "/api/products/add",
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(newProduct),
-        success: function(){
+        success: function () {
             window.location.replace("/productsPage")
         },
         error: function (error) {
